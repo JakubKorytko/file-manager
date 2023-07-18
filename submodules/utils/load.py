b53ad223e08e15c0b6f81920751de8db6bbdfdf1
JakubKorytko <jakub.korytko@o2.pl>
2023-07-18 23:57:15 +0200

    Change commands file name

--- submodules/utils/load.py
+++ submodules/utils/load.py
@@ -2,7 +2,7 @@ import os
 import json
 
 script_dir = os.path.dirname(__file__)
-txt_path = "data.json"
+txt_path = "config.json"
 file = os.path.join(script_dir, txt_path)
 
 jsonFile = open(file, "r")
